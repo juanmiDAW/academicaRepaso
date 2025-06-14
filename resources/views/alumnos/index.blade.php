2390d97a0e5708c3f76d544e697ff40513b83390
DAW <juanmiguel.valle@iesdonana.org>
2025-06-14 17:37:45 +0200

    paginacion en controlador y plantilla

--- resources/views/alumnos/index.blade.php
+++ resources/views/alumnos/index.blade.php
@@ -41,15 +41,15 @@
                                             </td>
 
                                             <td class="px-6 py-4">
-                            {{$alumno->telefono}}
+                                                {{ $alumno->telefono }}
                                             </td>
 
 
                                             {{--                                       
                                             <td class="px-6 py-4">
                                                 <a href="{{ route('alumnos.edit', $alumno) }}"
-                                                    class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"> --}}
-                                            Editar alumno
+                                                    class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"> 
+                                            Editar alumno--}}
                                             {{-- </a> --}}
                                             </td>
                                             {{-- <td class="px-6 py-4">
@@ -67,6 +67,8 @@
                                     @endforeach
                                 </tbody>
                             </table>
+                            
+                            {{ $alumnos->links() }}
                         </div>
                         <div class="mt-6 text-center">
                             <a href="{{ route('alumnos.create') }}"
